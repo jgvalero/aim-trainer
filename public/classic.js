bd94e4e6abbcf6711fe6183e9178992b7991e636
Jose Valero <jvalero000@gmail.com>
2023-09-16 13:09:46 -0700

    Added mechanics.

--- public/classic.js
+++ public/classic.js
@@ -1,4 +1,6 @@
 let gameStarted = false;
+let windowDimension = $(".window").css("width").slice(0, -2);
+let numCircles = 10;
 
 $(document).keypress(function () {
     console.log("Key pressed!");
@@ -9,20 +11,35 @@ $(document).keypress(function () {
 })
 
 function startGame() {
-    // Get width of window
-    let width = $(".window").css("width").slice(0, -2);
-    console.log(width);
-    let height = $(".window").css("height").slice(0, -2);
-    console.log(height);
-
-    let randomLocation = [Math.floor(Math.random() * (width - 50)), Math.floor(Math.random() * (width - 50))];
-    console.log(randomLocation);
-    let $circle = $("<div></div>").addClass("circle").css({ "top": randomLocation[0] + "px", "left": randomLocation[1] + "px" });
-    $(".window").append($circle);
+    $("h2").text(numCircles);
+    newCircle();
 }
 
-function makeCircle() {
+function newCircle() {
+    let randomLocation = [Math.floor(Math.random() * (windowDimension - 50)), Math.floor(Math.random() * (windowDimension - 50))];
+    console.log(randomLocation);
+    let $circle = $("<div></div>").addClass("circle").css({ "top": randomLocation[0] + "px", "left": randomLocation[1] + "px" });
 
+    $(".window").append($circle);
+    console.log("Created new circle");
+
+    listenCircleClick();
 }
 
 startGame();
+
+function listenCircleClick() {
+    $(".circle").click(function () {
+        numCircles--;
+        $("h2").text(numCircles);
+        $(this).remove();
+
+        if (numCircles === 0) {
+            endGame();
+        }
+        else {
+            newCircle();
+            console.log($(".circle"));
+        }
+    })
+}
